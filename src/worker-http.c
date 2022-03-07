c60c7cad987562c79350242aeeaf335e3669851e
Tara Mallesh <taramallesh@microsoft.com>
2022-03-07 21:16:11 +0000

    Fix typo to remove extra closing bracket

--- src/worker-http.c
+++ src/worker-http.c
@@ -885,7 +885,7 @@ int add_owasp_headers(worker_st * ws)
 		cstp_puts(ws, "Cross-Origin-Resource-Policy: same-origin\r\n") < 0 ||
 		cstp_puts(ws, "X-XSS-Protection: 0\r\n") < 0 ||
 		cstp_puts(ws, "Pragma: no-cache\r\n") < 0 ||
-		cstp_puts(ws, "Cache-control: no-store\r\n") < 0))
+		cstp_puts(ws, "Cache-control: no-store\r\n") < 0)
 	{
 		return -1;
 	}
