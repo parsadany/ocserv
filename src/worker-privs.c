3c105d7994f459ff80509612b5c97724df7b9b06
Zhiyuan Wan <h@iloli.bid>
2022-11-21 15:41:08 +0800

    seccomp: allow essential syscalls on 32-bit platform.
    This fix ocserv auth issue on armv7 platform.
    Not tested with other 32-bit platforms, be careful.
    
    Solves issue #474
    
    Signed-off-by: Zhiyuan Wan <h@iloli.bid>

--- src/worker-privs.c
+++ src/worker-privs.c
@@ -72,7 +72,7 @@ int set_sigsys_handler(struct worker_st *ws)
 }
 #endif
 
-
+#define SYSCALL_EXISTS(f) (defined(SYS_##f) || defined(__NR_##f))
 
 int disable_system_calls(struct worker_st *ws)
 {
@@ -122,8 +122,14 @@ int disable_system_calls(struct worker_st *ws)
 	ADD_SYSCALL(gettimeofday, 0);
 #if defined(HAVE_CLOCK_GETTIME)
 	ADD_SYSCALL(clock_gettime, 0);
+#if SYSCALL_EXISTS(clock_gettime64)
+	ADD_SYSCALL(clock_gettime64, 0);
+#endif
 #endif
 	ADD_SYSCALL(clock_nanosleep, 0);
+#if SYSCALL_EXISTS(clock_nanosleep64)
+	ADD_SYSCALL(clock_nanosleep64, 0);
+#endif
 	ADD_SYSCALL(nanosleep, 0);
 	ADD_SYSCALL(getrusage, 0);
 	ADD_SYSCALL(alarm, 0);
@@ -135,7 +141,7 @@ int disable_system_calls(struct worker_st *ws)
 	ADD_SYSCALL(brk, 0);
 	ADD_SYSCALL(mmap, 0);
 
-#ifdef __NR_getrandom
+#ifdef SYSCALL_EXISTS(getrandom)
 	ADD_SYSCALL(getrandom, 0); /* used by gnutls 3.5.x */
 #endif
 	ADD_SYSCALL(recvmsg, 0);
@@ -171,6 +177,9 @@ int disable_system_calls(struct worker_st *ws)
 
 	/* allow setting non-blocking sockets */
 	ADD_SYSCALL(fcntl, 0);
+#if SYSCALL_EXISTS(fcntl64)
+	ADD_SYSCALL(fcntl64, 0);
+#endif
 	ADD_SYSCALL(close, 0);
 	ADD_SYSCALL(exit, 0);
 	ADD_SYSCALL(exit_group, 0);
@@ -180,6 +189,9 @@ int disable_system_calls(struct worker_st *ws)
 	ADD_SYSCALL(openat, 0);
 	ADD_SYSCALL(fstat, 0);
 	ADD_SYSCALL(stat, 0);
+#if SYSCALL_EXISTS(fstat64)
+	ADD_SYSCALL(fstat64, 0);
+#endif
 	ADD_SYSCALL(stat64, 0);
 	ADD_SYSCALL(newfstatat, 0);
 	ADD_SYSCALL(lseek, 0);
