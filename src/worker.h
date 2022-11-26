30ae99084d6cb144ad10288780896c4fec0044aa
Nikos Mavrogiannopoulos <n.mavrogiannopoulos@gmail.com>
2022-11-26 16:47:04 +0100

    libtasn1: no longer use deprecated types
    Signed-off-by: Nikos Mavrogiannopoulos <n.mavrogiannopoulos@gmail.com>

--- src/worker.h
+++ src/worker.h
@@ -119,7 +119,7 @@ typedef struct dtls_ciphersuite_st {
 #ifdef HAVE_GSSAPI
 # include <libtasn1.h>
 /* main has initialized that for us */
-extern ASN1_TYPE _kkdcp_pkix1_asn;
+extern asn1_node _kkdcp_pkix1_asn;
 #endif
 
 struct http_req_st {
